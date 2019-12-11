151cf13b2e4a807c642305c3d62e9c9a96922e53
melia miller <meliajm@gmail.com>
2019-12-11 02:47:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 0 || integer == 1 || integer == 4
+    return false
+  end
+  result_array = []
+  array = (2...integer**0.5).to_a 
+  result_array << array.collect {|n| integer % n == 0 ? true:false}
+  result_array[0].include?(true) ? false : true
+end 
+  
+  
